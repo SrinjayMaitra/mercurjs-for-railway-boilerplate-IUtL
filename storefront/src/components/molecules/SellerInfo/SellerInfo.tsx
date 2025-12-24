3eb5b97be64b33ac2f6b712054a7558ecf342643
unknown <mihidebi@gmail.com>
2025-12-24 17:20:14 +0530

    Remove reviews section (ratings and review count) from SellerInfo component

--- storefront/src/components/molecules/SellerInfo/SellerInfo.tsx
+++ storefront/src/components/molecules/SellerInfo/SellerInfo.tsx
@@ -1,4 +1,3 @@
-import { StarRating } from "@/components/atoms"
 import { SellerAvatar } from "@/components/cells/SellerAvatar/SellerAvatar"
 import { SellerProps } from "@/types/seller"
 import { SellerReview } from "../SellerReview/SellerReview"
@@ -12,17 +11,6 @@ export const SellerInfo = ({
 }) => {
   const { photo, name, reviews } = seller
 
-  const reviewCount = reviews
-    ? reviews?.filter((rev) => rev !== null).length
-    : 0
-
-  const rating =
-    reviews && reviews.length > 0
-      ? reviews
-          .filter((rev) => rev !== null)
-          .reduce((sum, r) => sum + r?.rating || 0, 0) / reviewCount
-      : 0
-
   return (
     <div className="flex gap-4 w-full">
       <div className="relative h-12 w-12 overflow-hidden rounded-sm">
@@ -30,10 +18,6 @@ export const SellerInfo = ({
       </div>
       <div className="w-[90%]">
         <h3 className="heading-sm text-primary">{name}</h3>
-        <div className="flex items-center gap-2 border-b pb-4">
-          <StarRating starSize={16} rate={rating || 0} />
-          <span className="text-md text-secondary">{reviewCount} reviews</span>
-        </div>
         <div className="mt-4">
           {!header &&
             reviews
